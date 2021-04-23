a42ee867696029475132aacccbfd1a5ace388b68
Florian Bruggisser <florian@bildspur.ch>
2021-04-23 17:12:30 +0200

    added stopwatch

--- tests/load_all.py
+++ tests/load_all.py
@@ -7,13 +7,27 @@ import open3d as o3d
 from mesh_sequence_player.utils import get_files_in_path
 
 
+class Watch:
+    def __init__(self, name):
+        self.name = name
+
+    def __enter__(self):
+        self.start = time.time()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.end = time.time()
+        print("%s: %.4fs" % (self.name, self.end - self.start))
+
+
 def load(mesh_path, index: int = 0):
     x = index % 24 * 0.8
-    y = int(index / 24) * -1.7
+    y = int(index / 24) * -1.8
 
-    mesh = o3d.io.read_triangle_mesh(mesh_path)
+    with Watch("read"):
+        mesh = o3d.io.read_triangle_mesh(mesh_path)
     mesh.translate((x, y, 0))
-    vis.add_geometry(mesh, reset_bounding_box=True)
+    with Watch("add"):
+        vis.add_geometry(mesh, reset_bounding_box=True)
     print("loading %s" % mesh_path)
 
 
@@ -23,7 +37,7 @@ if __name__ == '__main__':
     args = a.parse_args()
 
     files = list(sorted(get_files_in_path(args.input, extensions=["*.obj"])))
-    files = [os.path.abspath(p) for p in files][:3] #[:168]
+    files = [os.path.abspath(p) for p in files][:5]  # [:168]
 
     vis = o3d.visualization.Visualizer()
 
