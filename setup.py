3bc0e7add5884fc21539749425fc78e5be428d42
Florian Bruggisser <florian@bildspur.ch>
2021-04-23 16:19:29 +0200

    prepared new release

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ required_packages = find_packages()
 
 setup(
     name=NAME,
-    version='1.2.0',
+    version='1.3.0',
     packages=required_packages,
     url='https://github.com/cansik/mesh-sequence-player',
     license='MIT License',
     author='Florian Bruggisser',
     author_email='github@broox.ch',
     description='A simple mesh sequence player based on open3d.',
-    install_requires=['wheel', 'open3d~=0.12.0'],
+    install_requires=['wheel', 'open3d~=0.12.0', 'opencv-python'],
 )
